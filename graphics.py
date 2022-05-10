169597f9d9d294e449948cda30dfaaf04d71f673
Hu's GitHub <alicehu0131@gmail.com>
2022-05-10 11:04:18 +0800

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -27,4 +27,46 @@ class Canvas(object):
 
 
 		self._tk = tk or tkinter.Tk()
-		
+		self._tk.protocol('WM_DELETE_WINDOW', sys.exit)
+		self._tk.title(title or 'Graphics Window')
+		self._tk.bind('<Button - 1>', self._click)
+		self._click_pos = None
+
+
+		self._canvas = tkinter.Canvas(self._tk, width = width, height = height)
+		self._canvas.pack()
+		self._draw_background()
+		self._canvas.update()
+		self._images = dict()
+
+
+
+	def clear(self, shape = 'all'):
+		self._canvas.delete(shape)
+		if shape == 'all':
+			self._draw_background()
+		self._canvas.update()
+
+
+
+	def draw_polygon(self, points, color = 'Black', fill_color = None, filled = 1, smooth = 0, width = 1):
+
+		if fill_color == None:
+			fill_color = color 
+		if filled == 0:
+			fill_color = ""
+
+		return self._canvas.create_polygon(flatterned(points), outline = color, fill = fill_color, smooth = smooth, width = width)
+
+
+
+
+
+
+
+
+
+
+
+
+
