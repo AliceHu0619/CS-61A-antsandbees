5c4a253af44e418dcff23d6e4e3d563751a70983
Hu's GitHub <alicehu0131@gmail.com>
2022-05-13 13:58:41 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -37,3 +37,79 @@ INSECT_FILES ={ 'Worker': ASSETS_DIR + INSECT_DIR +  "ant_harvester.gif",
        'Remover': ASSETS_DIR + INSECT_DIR + "remove.png",
 	
 }
+
+
+class GUI:
+
+
+	def __init__(self):
+		self.active = True
+		self.cleanState()
+
+
+	def cleanState(self):
+		self.initialized = False
+		self.state = state.State()
+		self.gameOver = False
+		self.colony = None
+		self.currentBeeId = 0
+		self.currentInsectId = 0
+		self.insects = []
+		self.bees = []
+		self.deadbees = []
+		self.deadinsects = []
+		self.insectToId  = {}
+		self.beeToId = {}
+		self.beelocations = {}
+
+
+
+
+	def makeHooks(self):
+		ants.Insect.reduce_armor = utils.class_method_wrapper(ants.Insect.reduce_armor, post = dead_insects)
+		ants.AntColony.remove_ant = utils.class_method_wrapper(ants.AntColony.remove_ant, post = remove_ant)
+
+
+
+	def newGameThread(self):
+		print('Trying to start new game')
+		self.cleanState()
+		importlib.reload(ants)
+		self.makeHooks()
+
+		self.winner = ants.start_with_strategy(gui.args, gui.strategy)
+		self.gameOver = True
+		self.saveState('winner', self.winner)
+		self.saveState('gameOver', self.gameOver)
+
+
+		update()
+
+
+	def killGUI(self):
+		self.active = False
+
+
+	def startGame(self, data = None):
+		threading.Thread(target = self.newGameThread).start()
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
