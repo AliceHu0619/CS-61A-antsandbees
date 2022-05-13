5aa85ef1e2ffc25e8a9be30541b3359b76990753
Hu's GitHub <alicehu0131@gmail.com>
2022-05-13 10:52:42 +0800

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -73,7 +73,63 @@ class Canvas(object):
 		x1, y1 = [c + radius for c in center]
 
 		return self._canvas.create_oval(x0, y0,x1, y1 outline = color, fill = fill_color,width = width)
+
+
+
+
+
+
+
+	def draw_line(self, start, end, color = 'Blue', width = 1):
+		x0, y0 = start
+		x1, y1 = end
+
+		return self._canvas.create_line(x0, y0, x1, y1. fill = color, width = width)
+
+
+
+
+	def draw_image(self, pos, image_file = None, scale = 1, anchor = tkinter.NW, behind = 0):
+
+		key = (image_file, scale)
+
+		if key not in self._images:
+			image = tkinter.PhotoImage(file = image_file)
+			if scale >= 1:
+				image = image.zoom(int(scale))
+
+			else:
+				image = image.subsample(int(1/scale))
+
+			self._images[key] = image
+
+
+
+		image = self._images[key]
+
+		x,y = pos 
+
+		id = self._canvas.create_image(x, y, image = image, anchor = anchor)
+
+		if behind > 0:
+			self._canvas.tag_lower(id, behind)
+
+
+		return id 
+
+
+
+	def draw_text(self, text, pos, color = 'Black', font = 'Arial', size =12, style = 'normal', anchor = tkinter.NW):
 		
+
+
+
+
+
+
+
+
+
 
 
 
